cd71c487f5d46b323a373048c267131eb8bb46fd
Peter Bickhofe <peter@bickhofe.de>
2018-06-25 13:03:38 +0200

    waypoints

---
+++ Assets/Scripts/GoToTarget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoToTarget : MonoBehaviour {
+
+    public Transform Target;
+    public float targetDist = .5f;
+    public bool canWalk = false;
+    public float speed = 10;
+    public float groundHeight = 1;
+
+    // Update is called once per frame
+    void Update () {
+        float dist = Vector3.Distance(transform.position, Target.position);
+        transform.LookAt(Target);
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+
+        if (canWalk && dist > targetDist)
+        {
+            transform.LookAt(Target);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, groundHeight, transform.position.z);   
+        }
+    }
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoints : MonoBehaviour {
+    public Vector3[] Ziele;
+    public float speed = 1;
+    int pos = 0;
+
+    void Update() {
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, Ziele[pos], step);
+
+		Vector3 targetDir = Ziele[pos] - transform.position;
+        //float step = speed * Time.deltaTime;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+        //Debug.DrawRay(transform.position, newDir, Color.red);
+        transform.rotation = Quaternion.LookRotation(newDir);
+
+
+		if (transform.position == Ziele[pos]) {
+			if (pos < Ziele.Length-1) pos++;
+			else pos = 0;
+		}
+    }
+}
