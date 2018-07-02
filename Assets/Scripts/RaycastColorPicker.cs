71f5f9fcc5332b92b68d9823b06c6c51143e4af4
Peter Bickhofe <peter@bickhofe.de>
2018-07-02 14:06:07 +0200

    2 juli pixel texture demos

---
+++ Assets/Scripts/RaycastColorPicker.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RaycastColorPicker : MonoBehaviour
+{
+    public Texture2D texture;
+    public Material material;
+    public Color color;
+    public Transform cube;
+
+
+    void Start()
+    {
+        texture = new Texture2D(512, 512);
+        material.mainTexture = texture;
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        
+        if (Physics.Raycast(transform.position, fwd, out hit, 100.0f))
+        {
+            cube.transform.position = hit.point;
+            print("Hit at: " + hit.point.x);
+
+            Vector2 pixelUV = hit.textureCoord;
+            pixelUV.x *= texture.width;
+            pixelUV.y *= texture.height;
+
+            texture.SetPixel((int)pixelUV.x, (int)pixelUV.y, Color.black);
+            //texture.SetPixel((int)hit.textureCoord.x, (int)hit.textureCoord.y, color);
+            texture.Apply();
+        }
+
+        else
+
+        {
+            print("no hit");
+        }
+        
+        //debug only
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 100;
+        Debug.DrawRay(transform.position, forward, Color.green);
+    }
+}
---
+++ Assets/Scripts/SetPixel.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetPixel : MonoBehaviour
+{
+    void Start()
+    {
+        Texture2D texture = new Texture2D(128, 128);
+        GetComponent<Renderer>().material.mainTexture = texture;
+
+
+        //pattern
+        //for (int y = 0; y < texture.height; y++)
+        //{
+        //    for (int x = 0; x < texture.width; x++)
+        //    {
+        //        Color color = ((x & y) != 0 ? Color.white : Color.gray);
+        //        texture.SetPixel(x, y, color);
+        //    }
+        //}
+
+        ////noise
+        //for (int y = 0; y < texture.height; y++)
+        //{
+        //    //Color color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+
+        //    for (int x = 0; x < texture.width; x++)
+        //    {
+        //        Color color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        //        texture.SetPixel(x, y, color);
+        //    }
+        //}
+
+        //lines
+        for (int y = 0; y < texture.height; y++)
+        {
+            Color color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+
+            for (int x = 0; x < texture.width; x++)
+            {
+                texture.SetPixel(x, y, color);
+            }
+        }
+
+        texture.Apply();
+    }
+}
