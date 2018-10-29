b3006a9c14f47af0f86cc751f2af879e73eae6c7
Peter Bickhofe <peter@bickhofe.de>
2018-10-29 13:26:04 +0100

    mesh

---
+++ Assets/Scripts/NewMesh.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMesh : MonoBehaviour
+{
+    Mesh mesh;
+    public Vector3[] Verticies;
+    public int[] Triangles;
+
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        CreateMesh();
+        UpdateMesh();
+    }
+
+
+    void CreateMesh()
+    {
+        Verticies = new Vector3[]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(0, 0, 1),
+            new Vector3(1, 0, 0)
+            //,
+            //new Vector3(1, 0, 1)
+        };
+
+        Triangles = new int[]
+        {
+            0,1,2
+            //,
+            //1,3,2
+        };
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = Verticies;
+        mesh.triangles = Triangles;
+
+        //mesh.RecalculateNormals();
+
+    }
+}
