0241fc3cb5f923fbd71c35947becbf5f6c01a91f
bickhofe <peter@bickhofe.de>
2019-01-06 11:00:53 +0100

    2d stuff

---
+++ 2D_Platformer/2D-Animation-master/2D Animation/Assets/fox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fox : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float jump = 25;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) rb.AddForce(transform.up * jump);
+	}
+}
